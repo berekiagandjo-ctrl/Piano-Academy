b03af41e6de6d6e5d4eb9fb6363ff3dd9c6b25c5
berekiagandjo-ctrl <berekiagandjo@gmail.com>
2026-08-18 22:44:37 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,57 @@
+const CACHE_NAME = "piano-academy-v1";
+
+const FILES = [
+    "./",
+    "./index.html",
+    "./theorie.html",
+    "./pratique.html",
+    "./guide.html",
+    "./lire-notes.html",
+    "./manifest.json",
+    "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(FILES))
+    );
+
+    self.skipWaiting();
+
+});
+
+
+self.addEventListener("activate", event => {
+
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+            )
+        )
+    );
+
+    self.clients.claim();
+
+});
+
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(response => {
+
+                return response ||
+                    fetch(event.request);
+
+            })
+
+    );
+
+});
